c8983b89eb43711f8337510f934d80ee646c2f93
Lilian Carvalho Santos Ribeiro <kleidk74@gmail.com>
2024-05-02 21:24:39 -0300

    responsiveness of empowering souls, completion of offerings and beginning of resilience

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -2,43 +2,102 @@ import Header from '../../Components/Header/Header';
 import './Home.css';
 
 const Home = () => {
-    return(
+    return (
         <>
-        <Header/>
-        <section className='highlight-home'>
-            <div className='text-highlight'>
+            <Header />
+            <section className='highlight-home'>
+                <div className='text-highlight'>
                     <h1>Reconecte-se às suas raízes com a <span>DJELI</span></h1>
                     <p>Seja Bem-vinda a um ambiente acolhedor</p>
                     <button>Explorar</button>
                 </div>
-        </section>
+            </section>
 
-        <section className='what-it-means'>
-            <img src="mulher-sorrindo.jpg" alt="Mulher sorrindo" />
-            <div className='text-what'>
-                <h2>O que é a Djeli?</h2>
+            <section className='what-it-means'>
+                <img src="mulher-sorrindo.jpg" alt="Mulher sorrindo" />
+                <div className='text-what'>
+                    <h2>O que é a Djeli?</h2>
+                    <p>Porem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur tempus urna at turpis condimentum lobortis. Ca aliquet odio mattis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur tempus urna at turpis condimentum lobortis.</p>
+                </div>
+            </section>
+
+            <section className='container-data'>
+                <p id='transformando'>Transformando cabelos</p>
+                <h2>Empoderando almas</h2>
+                <div className='datas'>
+                    <article className='data'>
+                        <h3>30%</h3>
+                        <p>Forem ipsum dolor sit amet</p>
+                    </article>
+                    <article className='data'>
+                        <h3>60%</h3>
+                        <p>Forem ipsum dolor sit amet</p>
+                    </article>
+                    <article className='data'>
+                        <h3>80%</h3>
+                        <p>Forem ipsum dolor sit amet</p>
+                    </article>
+                </div>
+            </section>
+
+            <section className='container-offering'>
+                <h2>O que a Djeli oferece?</h2>
+                <div className='resources'>
+                    <div className='container-offerings'>
+                        <div className='left-side'>
+                            <article className='offerings'>
+                                <div className='title-image'>
+                                    <img src="icon-community.svg" alt="Ícone de desenhos de pessoas em conjunto, representando uma comunidade" />
+                                    <div className='text-offerings'>
+                                        <h3>Rede de apoio</h3>
+                                        <img src="linha-esquerda.png" alt="Linha reta com detalhes laranjas" />
+                                    </div>
+                                </div>
+                                <p>Norem ipsum dolor sit amet, consectetur adipiscing elit, adip.</p>
+                            </article>
+                            <article className='offerings'>
+                                <div className='title-image'>
+                                    <img src="Money.svg" alt="Ícone notas de dinheiro empilhadas" />
+                                    <div className='text-offerings'>
+                                        <h3>Renda</h3>
+                                        <img src="linha-esquerda.png" alt="Linha reta com detalhes laranjas" />
+                                    </div>
+                                </div>
+                                <p>Norem ipsum dolor sit amet, consectetur adipiscing elit, adip.</p>
+                            </article>
+                        </div>
+                        <div className='right-side'>
+                            <article className='offerings'>
+                                <div className='title-image'>
+                                    <img src="icon-book.svg" alt="Ícone de um livro aberto" />
+                                    <div className='text-offerings'>
+                                        <h3>Aprendizado</h3>
+                                        <img src="linha-direita.png" alt="Linha reta com detalhes laranjas" />
+                                    </div>
+                                </div>
+                                <p>Norem ipsum dolor sit amet, consectetur adipiscing elit, adip.</p>
+                            </article>
+                            <article className='offerings'>
+                                <div className='title-image'>
+                                    <img src="heart.svg" alt="Ícone de coração" />
+                                    <div className='text-offerings'>
+                                        <h3>Acolhimento</h3>
+                                        <img src="linha-direita.png" alt="Linha reta com detalhes laranjas" />
+                                    </div>
+                                </div>
+                                <p>Norem ipsum dolor sit amet, consectetur adipiscing elit, adip.</p>
+                            </article>
+                        </div>
+                    </div>
+                    <img className='women-smiling' src="mulher-sorrindo-beneficios.jpg" alt="Mulher negra cabelo crespo sorrindo e olhando para a esquerda. Fundo laranja com detalhes que remetem a África" />
+                </div>
+            </section>
+
+            <section className='resilience'>
+                <p>Cachos que contam histórias de</p>
+                <h2>Resiliência, força e poder</h2>
                 <p>Porem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur tempus urna at turpis condimentum lobortis. Ca aliquet odio mattis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur tempus urna at turpis condimentum lobortis.</p>
-            </div>
-        </section>
-
-        <section className='container-data'>
-            <p id='transformando'>Transformando cabelos</p>
-            <h2>Empoderando almas</h2>
-            <div className='datas'>
-                <article className='data'>
-                    <h3>30%</h3>
-                    <p>Forem ipsum dolor sit amet</p>
-                </article>
-                <article className='data'>
-                    <h3>60%</h3>
-                    <p>Forem ipsum dolor sit amet</p>
-                </article>
-                <article className='data'>
-                    <h3>80%</h3>
-                    <p>Forem ipsum dolor sit amet</p>
-                </article>
-            </div>
-        </section>
+            </section>
         </>
     )
 }
