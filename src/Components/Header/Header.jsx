1b50ef5948806d2eb4d894c216955a23574fd8d1
Lilian Carvalho Santos Ribeiro <kleidk74@gmail.com>
2024-05-03 12:19:27 -0300

    Adjusting the navbar links

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -9,14 +9,14 @@ const Header = () => {
   return (
     <Navbar className="navbar-header" expand="lg">
       <Container className='container-header-general'>
-        <Navbar.Brand href="/" className='brand'><img src='logo-djeli-laranja.svg' alt='Logo da Djeli Laranja'></img></Navbar.Brand>
+        <Navbar.Brand to="/" className='brand'><img src='logo-djeli-laranja.svg' alt='Logo da Djeli Laranja'></img></Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto brand">
-          <Nav.Link href="/">Início</Nav.Link>
-          <Nav.Link href="/sobrenos">Sobre nós</Nav.Link>
-          <Nav.Link href="#parceiros">Parceiros</Nav.Link>
-          <Nav.Link href="/faleconosco">Fale conosco</Nav.Link>
+            <Link to="/">Início</Link>
+            <Link to="/sobrenos">Sobre nós</Link>
+            <Link to="#parceiros">Parceiros</Link>
+            <Link to="/faleconosco">Fale conosco</Link>
           </Nav>
           <Nav className='ms-auto container-buttons-header'>
             <Link to="/login"><button type="submit" id="button-login">Login</button></Link>
